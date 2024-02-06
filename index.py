7892d9897be3f5a1defbe9bcfbebeab8b8ffb560
msteja2023 <144339469+msteja2023@users.noreply.github.com>
2024-02-06 21:28:44 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,55 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 5)
+
+
+def check_winner(board):
+    for row in board:
+        if row.count(row[0]) == len(row) and row[0] != ' ':
+            return True
+
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] != ' ':
+            return True
+
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return True
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return True
+
+    return False
+
+
+def tic_tac_toe():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    player = 'X'
+
+    print("Welcome to Tic-Tac-Toe!")
+
+    while True:
+        print_board(board)
+        row = int(input("Enter row (1, 2, or 3): ")) - 1
+        col = int(input("Enter column (1, 2, or 3): ")) - 1
+
+        if board[row][col] == ' ':
+            board[row][col] = player
+        else:
+            print("That spot is already taken! Try again.")
+            continue
+
+        if check_winner(board):
+            print_board(board)
+            print(f"Player {player} wins!")
+            break
+
+        if all(board[i][j] != ' ' for i in range(3) for j in range(3)):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        player = 'O' if player == 'X' else 'X'
+
+
+if __name__ == "__main__":
+    tic_tac_toe()
